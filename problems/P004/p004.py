534bbdfcbd5f72d87653dc493ad7163e217f8734
Hanyeh afshin <afshinhanyeih@gmail.com>
2023-11-19 06:19:46 +0330

    we

---
+++ problems/P004/p004.py
@@ -0,0 +1,22 @@
+r=int(input())
+if r==1 :
+    print("one")
+if  r==2 :
+    print("two")    
+if r ==3: 
+    print("three")
+if r== 4 :
+    print("four")
+if r==5 :
+    print("five")
+if r==6 :
+    print(" six")
+if r==7 :
+    print("seven")
+if r ==8:
+    print("eight")
+if r==9 :
+    print("nine")
+if r==10:
+    print("ten")                    
+    
